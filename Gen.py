6c978fa337ea807baddb70407638689a62109c5c
Inletbr <xfay023@gmail.com>
2022-09-27 14:01:02 +0300

    Initial commit

---
+++ Gen.py
@@ -0,0 +1,42 @@
+import random
+
+
+
+login = input("Enter your login: \n")
+password = ""
+lenght = int(input("Enter password lenght: \n"))
+answerbool = True
+answlist = ["y", "n"]
+
+while answerbool:
+    answer = input("Use special charater?: y/n \n")
+    if answer not in answlist:
+        print("Incorrect answer!")
+    elif answer in answlist:
+        answerbool = False
+
+
+liblistws = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+             "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+             "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
+             "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R",
+             "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F",
+             "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B",
+             "N", "M", "~", "!", "@", "$", "%", "^", "&", "*", "(", ")"]
+liblist = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+             "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+             "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
+             "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R",
+             "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F",
+             "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B",
+             "N", "M",]
+
+if answer == "y":
+    for x in range(lenght):
+        password = password + random.choice(liblistws)
+elif answer == "n":
+    for x in range(lenght):
+        password = password + random.choice(liblist)
+
+
+print(f"Login:", login, "\nPassword:", password)
