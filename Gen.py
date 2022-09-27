90d1ecd0efb20757de16f2a5c18b53ea09e1def6
Inletbr <xfay023@gmail.com>
2022-09-27 14:19:04 +0300

    1

--- Gen.py
+++ Gen.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 login = input("Enter your login: \n")
 password = ""
 lenght = int(input("Enter password lenght: \n"))
@@ -15,7 +13,6 @@ while answerbool:
     elif answer in answlist:
         answerbool = False
 
-
 liblistws = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
              "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
              "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
@@ -24,19 +21,18 @@ liblistws = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
              "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B",
              "N", "M", "~", "!", "@", "$", "%", "^", "&", "*", "(", ")"]
 liblist = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-             "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-             "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
-             "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R",
-             "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F",
-             "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B",
-             "N", "M",]
+           "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+           "a", "s", "d", "f", "g", "h", "j", "k", "l", "z",
+           "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R",
+           "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F",
+           "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B",
+           "N", "M"]
 
 if answer == "y":
-    for x in range(lenght):
+    for i in range(lenght):
         password = password + random.choice(liblistws)
 elif answer == "n":
-    for x in range(lenght):
+    for i in range(lenght):
         password = password + random.choice(liblist)
 
-
-print(f"Login:", login, "\nPassword:", password)
+print(f"Login:", login, "\nPassword:", password, "\n")
